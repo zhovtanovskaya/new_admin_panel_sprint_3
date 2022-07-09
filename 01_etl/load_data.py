b107bdc099e5f522e33761d5bac99d494e5d3e78
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-07-09 22:21:15 +0600

    В load_data.py реализовано заполнение индекса ElasticSearch из PostgreSQL

--- 01_etl/load_data.py
+++ 01_etl/load_data.py
@@ -2,11 +2,14 @@
 
 import settings
 from db_objects import FilmWork
+from elastic_search_saver import ElasticSearchSaver, elastic_search_connection
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import RequestError
 from postgres_loader import PostgresLoader, postgres_connection
 from psycopg2.extensions import connection as pg_connection
 
 
-def load(pg_conn: pg_connection, es_conn=None) -> None:
+def load(pg_conn: pg_connection, es_client: Elasticsearch) -> None:
     """Основной метод загрузки данных из SQLite в Postgres.
 
     Args:
@@ -14,12 +17,22 @@ def load(pg_conn: pg_connection, es_conn=None) -> None:
         pg_conn: подключение к базе приемнику данных в PostgreSQL.
     """
     loader = PostgresLoader(pg_conn)
+    saver = ElasticSearchSaver(es_client)
     for row in loader.load():
         obj = FilmWork(**row)
+        doc = obj.as_document()
+        try:
+            saver.save(doc)
+        except RequestError as e:
+            print(e)
+            import pprint
+            pprint.pprint(doc)
+            break
 
 
 if __name__ == '__main__':
     with (
         postgres_connection(settings.POSTGRES_DB) as pg_conn,
+        elastic_search_connection(settings.ELASTIC_HOST) as es_client,
     ):
-            load(pg_conn)
+        load(pg_conn, es_client)
