8e1e2623f4340dda5e1d72f5d6cc0ed835e12217
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-07-10 16:26:04 +0600

    Добавлен storage.State с тестами

---
+++ 01_etl/storage.py
@@ -0,0 +1,53 @@
+import abc
+import json
+from typing import Any, Optional
+
+
+class BaseStorage:
+    @abc.abstractmethod
+    def save_state(self, state: dict) -> None:
+        """Сохранить состояние в постоянное хранилище"""
+        pass
+
+    @abc.abstractmethod
+    def retrieve_state(self) -> dict:
+        """Загрузить состояние локально из постоянного хранилища"""
+        return {}
+
+
+class JsonFileStorage(BaseStorage):
+    def __init__(self, file_path: Optional[str] = None):
+        self.file_path = file_path
+
+    def save_state(self, state: dict) -> None:
+        with open(self.file_path, 'w') as f:
+            f.write(json.dumps(state))
+
+    def retrieve_state(self) -> dict:
+        with open(self.file_path) as f:
+            state = f.read()
+        return json.loads(state)
+
+
+class State:
+    """
+    Класс для хранения состояния при работе с данными, чтобы постоянно не
+    перечитывать данные с начала.
+    Здесь представлена реализация с сохранением состояния в файл.
+    В целом ничего не мешает поменять это поведение на работу с БД или
+    распределённым хранилищем.
+    """
+
+    def __init__(self, storage: BaseStorage):
+        self.storage = storage
+        self._state = {}
+
+    def set_state(self, key: str, value: Any) -> None:
+        """Установить состояние для определённого ключа"""
+        self._state[key] = value
+        self.storage.save_state(self._state)
+
+    def get_state(self, key: str) -> Any:
+        """Получить состояние по определённому ключу"""
+        self._state = self.storage.retrieve_state()
+        return self._state.get(key)
