7578d6862d7ff40b64c8ed6e8cd59f7fd74f44d8
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-08-01 09:03:54 +0600

    Использован @backoff в load_data.py

--- 01_etl/load_data.py
+++ 01_etl/load_data.py
@@ -1,8 +1,10 @@
 """Основной модуль для импорта кино из PostgreSQL в ElasticSearch."""
 
+import time
+
 import settings
+from availability.backoff import backoff
 from elasticsearch import Elasticsearch
-from elasticsearch.exceptions import RequestError
 from extract.postgres_loader import PostgresLoader, postgres_connection
 from load.elastic_search_saver import (ElasticSearchSaver,
                                        elastic_search_connection)
@@ -22,28 +24,27 @@ def load(
         state: файл для хранения состояния синхронизации ElasticSearch.
     """
     SINCE_KEY = 'since'
-
     loader = PostgresLoader(pg_conn)
     saver = ElasticSearchSaver(es_client)
     for row in loader.load(since=state.get_state(SINCE_KEY)):
         obj = FilmWork(**row)
         doc = obj.as_document()
-        try:
-            saver.save(doc)
-        except (RequestError, KeyboardInterrupt) as e:
-            print(e)
-            import pprint
-            pprint.pprint(doc)
-            break
-        else:
-            state.set_state(SINCE_KEY, obj.modified)
-        break
+        saver.save(doc)
+        state.set_state(SINCE_KEY, obj.modified)
+        print(obj.title)
+        time.sleep(2)
+        # break
 
 
-if __name__ == '__main__':
+@backoff()
+def etl():
     with (
         postgres_connection(settings.POSTGRES_DB) as pg_conn,
         elastic_search_connection(settings.ELASTIC_HOST) as es_client,
     ):
         storage = JsonFileStorage(settings.STATE_FILE)
         load(pg_conn, es_client, State(storage))
+
+
+if __name__ == '__main__':
+    etl()
