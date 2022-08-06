164f437c2a7d517c10057b44b32784e1db78a094
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-08-06 19:18:33 +0600

    Добавлен метод ElasticSearchSaver.is_batch_ready()

--- 01_etl/load/elastic_search_saver.py
+++ 01_etl/load/elastic_search_saver.py
@@ -39,7 +39,7 @@ def elastic_search_connection(host: dict) -> Elasticsearch:
 class ElasticSearchSaver:
     """Загрузчик фильмов в индекс ElasticSearch."""
 
-    def __init__(self, es_client: Elasticsearch):
+    def __init__(self, es_client: Elasticsearch, batch_size=100):
         """Инициализация атрибутов класса.
 
         Args:
@@ -48,6 +48,7 @@ class ElasticSearchSaver:
         self.client = es_client
         self.index = 'movies'
         self._documents = []
+        self._batch_size = batch_size
 
     def save(self, document: dict) -> dict[str, Any]:
         """Создать или обновить документ.
@@ -56,6 +57,9 @@ class ElasticSearchSaver:
             document: Документ для Elastic Search.
         """
         self._documents.append(document)
+
+    def is_batch_ready(self):
+        return len(self._documents) >= self._batch_size
 
     def flush(self):
         def get_actions(documents):
@@ -67,7 +71,8 @@ class ElasticSearchSaver:
                     "_source": document,
                 }
                 yield action
-        return helpers.bulk(self.client, get_actions(self._documents))
+        helpers.bulk(self.client, get_actions(self._documents))
+        self._documents = []
 
     def get(self, id: uuid.UUID) -> Union[dict, None]:
         """Получить документ по id.
