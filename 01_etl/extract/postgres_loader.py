825d9f65ab21880e7d7128d7f08c619b25447f6f
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-08-07 22:37:43 +0600

    Удален метод PostgresLoader.load()

--- 01_etl/extract/postgres_loader.py
+++ 01_etl/extract/postgres_loader.py
@@ -69,48 +69,6 @@ class PostgresLoader:
                 for row in rows:
                     yield row
 
-    def load(self, since: str = EPOCH) -> RealDictRow:
-        """Получить фильм из PostgreSQL один за другим.
-
-        Args:
-            since: Получить строки, у которых дата правки больше строго since.
-
-        Yields:
-            Строка, представляющая фильм с жанрами и персонами.
-        """
-        sql = """
-            SELECT
-                fw.id,
-                fw.title,
-                fw.description,
-                fw.rating,
-                fw.type,
-                fw.created,
-                fw.modified,
-                COALESCE (
-                   json_agg(
-                       DISTINCT jsonb_build_object(
-                           'role', pfw.role,
-                           'id', p.id,
-                           'name', p.full_name
-                       )
-                   ) FILTER (WHERE p.id is not null),
-                   '[]'
-                ) as persons,
-                json_agg(DISTINCT g.name) as genres
-            FROM content.film_work fw
-            LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
-            LEFT JOIN content.person p ON p.id = pfw.person_id
-            LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
-            LEFT JOIN content.genre g ON g.id = gfw.genre_id
-            WHERE fw.modified > %s
-            GROUP BY fw.id
-            ORDER BY fw.modified;
-        """
-        values = (since or self.EPOCH,)
-        rows = self._execute_sql(sql, values)
-        yield from rows
-
     def load_all(self):
         genre_since = self.state.get_state('genre_since') or self.EPOCH
         for ids, genre_since in self.ids_for_genre_since(genre_since):
