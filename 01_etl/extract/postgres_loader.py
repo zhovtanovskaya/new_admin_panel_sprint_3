4d4e55eaf7e63c0469c199e76e954a42c2c56055
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-08-07 22:11:53 +0600

    Добавлена загрузка обновлений из person в PostgresLoader.load_all()

--- 01_etl/extract/postgres_loader.py
+++ 01_etl/extract/postgres_loader.py
@@ -116,7 +116,13 @@ class PostgresLoader:
         for ids, genre_since in self.ids_for_genre_since(genre_since):
             yield from self.get_film_works(ids)
             self.state.set_state('genre_since', genre_since)
-            print(genre_since)
+            print('genre_since', genre_since)
+        person_since = self.state.get_state('person_since') or self.EPOCH
+        for ids, person_since in self.ids_for_person_since(person_since):
+            yield from self.get_film_works(ids)
+            self.state.set_state('person_since', person_since)
+            print('person_since', person_since)
+        
 
     def _rows_for_genre_since(
         self, since: str = EPOCH, bunch_size: int = 100,
@@ -134,22 +140,47 @@ class PostgresLoader:
             ORDER BY min_modified, gfw.film_work_id;
         """
         values = (since,)
-        rows = self._execute_sql(sql, values)
+        yield from self._bunchify(self._execute_sql(sql, values))
+
+    def _rows_for_person_since(
+        self, since: str = EPOCH,
+    ) -> list[RealDictRow]:
+        sql = """
+            SELECT
+                pfw.film_work_id,
+                min(p.modified) min_modified
+            FROM person p
+            INNER JOIN person_film_work pfw ON p.id = pfw.person_id
+            WHERE p.modified >= %s
+            GROUP BY pfw.film_work_id
+            ORDER BY min_modified, pfw.film_work_id;
+        """
+        values = (since,)
+        yield from self._bunchify(self._execute_sql(sql, values))
+
+    def ids_for_genre_since(self, since):
+        gen = self._rows_for_genre_since(since)
+        yield from self._split_rows(gen)
+
+    def ids_for_person_since(self, since):
+        gen = self._rows_for_person_since(since)
+        yield from self._split_rows(gen)
+
+    def _split_rows(self, bunches):
+        for bunch in bunches:
+            fw_ids, modified_dates = zip(*(row.values() for row in bunch))
+            genre_since = modified_dates[0]
+            yield fw_ids, genre_since
+
+    def _bunchify(self, gen, bunch_size: int = 100):
         bunch = []
-        for row in rows:
+        for row in gen:
             bunch.append(row)
             if len(bunch) >= bunch_size:
                 yield bunch
                 bunch = []
         if bunch:
             yield bunch
-
-    def ids_for_genre_since(self, since):
-        genres = self._rows_for_genre_since(since)
-        for rows in genres:
-            fw_ids, modified_dates = zip(*(row.values() for row in rows))
-            genre_since = modified_dates[0]
-            yield fw_ids, genre_since
 
     def get_film_works(self, ids):
         sql = """
