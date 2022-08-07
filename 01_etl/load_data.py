ee2ab98db3ff20b54656bef415789386500080c7
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-08-07 11:19:45 +0600

    Рефакторинг в load_data.py
    Создание saver'а и loader'а происходит теперь в etl(),
    а не в loader().

--- 01_etl/load_data.py
+++ 01_etl/load_data.py
@@ -1,35 +1,31 @@
 """Основной модуль для импорта кино из PostgreSQL в ElasticSearch."""
 
+from elasticsearch.exceptions import ConnectionError
+from psycopg2 import OperationalError
+
 import settings
 from availability.backoff import backoff
-from elasticsearch import Elasticsearch
-from elasticsearch.exceptions import ConnectionError
 from extract.postgres_loader import PostgresLoader, postgres_connection
 from load.elastic_search_saver import (ElasticSearchSaver,
                                        elastic_search_connection)
-from psycopg2 import OperationalError
-from psycopg2.extensions import connection as pg_connection
 from storage import JsonFileStorage, State
 from transform.db_objects import FilmWork
 
 
 def load(
-        pg_conn: pg_connection, es_client: Elasticsearch, state: State,
+        loader: PostgresLoader, saver: ElasticSearchSaver, state: State,
         ) -> None:
     """Для каждой строки фильма из PostgreSQL создать документ в ElasticSearch.
 
     Args:
-        pg_conn: подключение к базе приемнику данных в PostgreSQL.
-        es_client: подключение к ElasticSearch.
+        loader: загрузчик фильмов из PostgreSQL.
+        saver: загрузчик фильмов в ElasticSearch.
         state: файл для хранения состояния синхронизации ElasticSearch.
     """
     SINCE_KEY = 'since'
-    loader = PostgresLoader(pg_conn)
-    saver = ElasticSearchSaver(es_client)
     for row in loader.load(since=state.get_state(SINCE_KEY)):
         obj = FilmWork(**row)
-        doc = obj.as_document()
-        saver.save(doc)
+        saver.save(obj.as_document())
         if saver.is_batch_ready():
             saver.flush()
             state.set_state(SINCE_KEY, obj.modified)
@@ -44,8 +40,10 @@ def etl() -> None:
         postgres_connection(settings.POSTGRES_DB) as pg_conn,
         elastic_search_connection(settings.ELASTIC_HOST) as es_client,
     ):
+        loader = PostgresLoader(pg_conn)
+        saver = ElasticSearchSaver(es_client)
         storage = JsonFileStorage(settings.STATE_FILE)
-        load(pg_conn, es_client, State(storage))
+        load(loader, saver, State(storage))
 
 
 if __name__ == '__main__':
